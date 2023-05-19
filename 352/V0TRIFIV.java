8a12fd550c1421be1848f19144bb285fb3c337bf
Marco Antonio Hernandez Rocha <130703920+marcoaprende@users.noreply.github.com>
2023-05-19 15:02:00 -0600

    Update V0TRIFIV.java

--- 352/V0TRIFIV.java
+++ 352/V0TRIFIV.java
@@ -1,17 +1,40 @@
 
 
 package com.mycompany.multiplo;
-
-
 public class Multiplo {
+package com.mycompany.v1trifiv;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+public class V1TRIFIV {
 
-    public static void main(String[] args) {
-        for(int o=0; o<=100; o++){
+    public static void main(String[] args) throws IOException {
+        //declaracion de datos
+        int numero1;
+        int multa3;
+        int multa5;
+        BufferedReader bufEntrada= new BufferedReader(new InputStreamReader(System.in));
+   
+        //entrada de datos
+        System.out.println("dame el numero");
+        numero1= Integer.parseInt(bufEntrada.readLine());
         
-   if(o%3==0){
-        System.out.println(o+ "el multiplo es Try");
-    }else if (o%5==0){
-        System.out.println(o+"El multiplo es Fiv");
+        //Proceso de salida
+        multa3= numero1%3;
+        multa5= numero1%5;
+        //salida de datos
+        if(multa3==0){ System.out.println("Try");
+        
+        }if(multa5==0){ System.out.println("Five");
+        
+        }if(multa3==0 && multa5== 0){ System.out.println("try five");
+        }
+        
+
+    }
+    }
+    
+   
 
     }
     }
