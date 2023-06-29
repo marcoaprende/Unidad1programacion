48b3f15da28dadbcc9432f9380e4d607385f9bf4
Marco Antonio Hernandez Rocha <130703920+marcoaprende@users.noreply.github.com>
2023-06-29 09:22:43 -0600

    2-2Descuento.java

--- unidad2/2-2CalcularMonto/src/main/java/com/mycompany/calcularmonto/App.java
+++ unidad2/2-2CalcularMonto/src/main/java/com/mycompany/calcularmonto/App.java
@@ -1,59 +1,47 @@
-
-
-package com.mycompany.calcularmonto;
-
-
+package com.mycompany.descuentos;
 import java.util.Scanner;
 
-public class App {
+public class Descuentos {
 
     public static void main(String[] args) {
-       //Definicion de Variables.
-       double PV =1000;
-       Scanner entrada = new Scanner(System.in);
-       int descuento=0;
-       double preciofinal;
-       String codPromocion;
-       
-       //Entrada de Datos
-       System.out.println("Su total de compra es: $"+ PV + "\n");
-       System.out.println("Tines un codigo de descuento Escribelo:");
-       codPromocion = entrada.next();
+        // Definicion de variables
+        double PV = 1000;
+        Scanner entrada = new Scanner(System.in);
+        int descuento = 0;
+        double precioFinal;
+        String codPromocion;
+        
+        // Entrada de datos
+        System.out.println("Su total de compra es: $" + PV + "\n");
+        System.out.println("Tienes un codigo de descuento, escríbelo:");
+        codPromocion = entrada.next();
         codPromocion = codPromocion.toUpperCase();
-        
-        //Proceso.
-        
-        //Salida de Datos
-        
-        switch(codPromocion){
-            case  "PlatiOctubre":
-              descuento= 20;
-              break;
-            case  "SilverOctubre":  
-              descuento= 50;
-              break;
-            case "BronceOctubre":
-              descuento = 35;
-              break;
-            case "OroOctubre":
-               descuento =75;
-               break;
+         
+        // Proceso
+        // Salida de datos
+        switch(codPromocion) {
+            case "PLATINOOCTUBRE":
+                descuento = 20;
+                break; 
+               
+            case "SILVEROCTUBRE":
+                descuento = 50; 
+                break;
+             
+            case "BRONCEOCTUBRE":
+                descuento = 35;
+                break;
+                
+            case "OROOCTUBRE":
+                descuento = 75;
+                break;    
+                
             default:
-               System.out.println("Has introsucido un codigo no valido:"); 
+                System.out.println("Se ha introducido un código de descuento inválido.");
+                break;
         }
-        preciofinal = PV -descuento;
-                   
         
+        precioFinal = PV - (PV * descuento / 100); // Restar el descuento al precio original
+        System.out.println("El total con el descuento aplicado es: $" + precioFinal);
     }
 }
-
-  
-        
-        
-        
-        
-        
-        
-        
-        
-  
